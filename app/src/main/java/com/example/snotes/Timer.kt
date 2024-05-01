56bd89f7b99bd0414421418f5a8683d4d57e19a5
shambu <shambuchandrannair@gmail.com>
2024-05-01 14:02:03 +0530

    fourth commit

---
+++ app/src/main/java/com/example/snotes/Timer.kt
@@ -0,0 +1,44 @@
+package com.example.snotes
+
+import android.os.Handler
+import android.os.Looper
+
+class Timer(listener: OnTimerChangeListener) {
+    interface OnTimerChangeListener{
+        fun onTimerChange(duration: String)
+    }
+    private var handler =Handler(Looper.getMainLooper())
+    private lateinit var runnable:Runnable
+
+    private var duration = 0L
+    private var delay= 100L
+    init {
+        runnable= Runnable {
+            duration += delay
+            handler.postDelayed(runnable,delay)
+            listener.onTimerChange(format())
+        }
+    }
+     fun start(){
+        handler.postDelayed(runnable,delay)
+    }
+     fun pause(){
+        handler.removeCallbacks(runnable)
+    }
+     fun stop(){
+        handler.removeCallbacks(runnable)
+        duration=0L
+    }
+    fun format():String{
+        val millis=(duration %1000)/10
+        val seconds=(duration/1000)%60
+        val minutes =(duration/(1000*60))%60
+        val hours=(duration/(1000*60*60))
+        var timeFormat =if(hours>0)
+            "%02d:%02d:%02d:%02d".format(hours,minutes,seconds,millis)
+        else
+            "%02d:%02d:%02d".format(minutes,seconds,millis)
+            return timeFormat
+    }
+
+}
--- app/src/main/java/com/example/snotes/audiorecd.kt
+++ app/src/main/java/com/example/snotes/audiorecd.kt
@@ -1,6 +1,8 @@
 package com.example.snotes
 
 import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
+import android.media.MediaRecorder
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
@@ -9,32 +11,48 @@ import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.snotes.databinding.ActivityAudiorecdBinding
+import java.io.IOException
+import java.util.Date
 
-class audiorecd : AppCompatActivity() {
+class audiorecd : AppCompatActivity() ,Timer.OnTimerChangeListener{
 
     private var permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
-    private var permissionGranted =false
-    val REQUEST_CODE=5
+    private var permissionGranted = false
+    val REQUEST_CODE = 5
     private lateinit var binding: ActivityAudiorecdBinding
-    private lateinit var recordbutton:ImageButton
+    private lateinit var recordbutton: ImageButton
+    private lateinit var recorder: MediaRecorder
+    private var dirpath = ""
+    private var filename = ""
+    private var isrecording = false
+    private var ispaused = false
+    private lateinit var timer: Timer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding= ActivityAudiorecdBinding.inflate(layoutInflater)
+        binding = ActivityAudiorecdBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        permissionGranted=ActivityCompat.checkSelfPermission(this,permissions[0]) == PackageManager.PERMISSION_GRANTED
-        if (!permissionGranted){
-            ActivityCompat.requestPermissions(this,permissions,REQUEST_CODE)
+        permissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            permissions[0]
+        ) == PackageManager.PERMISSION_GRANTED
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
         }
-        recordbutton=binding.btnrecord
+        recordbutton = binding.btnrecord
         recordbutton.setOnClickListener {
-            startRecording()
+            when {
+                ispaused -> resumeRecording()
+                isrecording -> pauseRecording()
+                else -> startRecording()
+            }
         }
+        timer= Timer(this)
 
     }
 
@@ -44,14 +62,57 @@ class audiorecd : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE){
-            permissionGranted =grantResults[0] ==PackageManager.PERMISSION_GRANTED
+        if (requestCode == REQUEST_CODE) {
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
         }
     }
-    private fun startRecording(){
-        if (!permissionGranted){
-            ActivityCompat.requestPermissions(this,permissions,REQUEST_CODE)
-        }
 
+    private fun resumeRecording() {
+        recorder.resume()
+        ispaused = false
+        recordbutton.setImageResource(R.drawable.round_pause_24)
+        timer.start()
+    }
+
+    private fun pauseRecording() {
+        recorder.pause()
+        ispaused = true
+        recordbutton.setImageResource(R.drawable.ic_record)
+        timer.pause()
+    }
+    private fun stopRecording() {
+
+        timer.stop()
+    }
+
+    private fun startRecording() {
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+        }
+        recorder = MediaRecorder()
+        dirpath = "${externalCacheDir?.absolutePath}/"
+        var simpleDateFormat = SimpleDateFormat("yyyy.MM.DD_hh.mm.ss")
+        var date = simpleDateFormat.format(Date())
+        filename = "audio_rec_$date"
+        recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirpath$filename.mp3")
+            try {
+                prepare()
+            } catch (e: IOException) {
+            }
+            start()
+        }
+        recordbutton.setImageResource(R.drawable.round_pause_24)
+        isrecording = true
+        ispaused = false
+        timer.start()
+
+    }
+
+    override fun onTimerChange(duration: String) {
+      binding.tvtimer.text=duration
     }
 }
